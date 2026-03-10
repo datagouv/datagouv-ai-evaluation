4837ca93f1a7b183f7116c0377ceec9aaddffb66
estelle <estelle.btd@gmail.com>
2026-03-10 15:40:18 +0100

    chore: add one more evaluator

--- mcp_eval/experiment/evaluators.py
+++ mcp_eval/experiment/evaluators.py
@@ -4,13 +4,32 @@ from phoenix.evals import create_evaluator
 
 @create_evaluator(name="tool_selection_accuracy", kind="code", direction="maximize")
 def tool_selection_accuracy(expected, output):
-    expected_tools = [
+    expected_tool_names = [
         tool["name"] for tool in ast.literal_eval(expected["expected_tool_calls"])
     ]
 
-    correct_tools = [
+    correct_tool_names = [
         tool["name"]
         for tool in output["actual_tool_calls"]
-        if tool["name"] in expected_tools
+        if tool["name"] in expected_tool_names
     ]
-    return len(correct_tools) / len(expected_tools)
+    return len(correct_tool_names) / len(expected_tool_names)
+
+
+"""
+@create_evaluator(name="tool_call_accuracy", kind="code", direction="maximize")
+def tool_call_accuracy(expected, output):
+    expected_tools = ast.literal_eval(expected["expected_tool_calls"])
+    expected_tool_names = [
+        tool["name"] for tool in expected_tools
+    ]
+    for actual_tool in output["actual_tool_calls"]:
+        actual_tool_name = actual_tool["name"]
+        if actual_tool_name in expected_tool_names:
+            for key, value in actual_tool.items():
+                if key in expected_tools[actual_tool_name]
+                
+                
+            
+    
+    return len(correct_tools) / len(expected_tools)"""
